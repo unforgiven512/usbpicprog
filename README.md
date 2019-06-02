26182f79748726647f9a83f1cc60b6ad3c5086f1
Gerad Munsch <gmunsch@unforgivendevelopment.com>
2019-06-02 13:13:17 -0500

    made the README.md a bit more comprehensive, and performed some quoting and formatting on trunk/release.sh

--- README.md
+++ README.md
@@ -1,14 +1,48 @@
-#Usbpicprog GIT Repository
+# usbpicprog #
 
-This is the official GIT repository usbpicprog, the open source usb pic programmer.
+This is a fork of the [usbpicprog][1] project, featuring various enhancements and updates. The original [usbpicprog][1]
+project is featured as an _open-source_ **USB** PIC programmer; the original git repository is found [here][2].
 
-For more information please visit: [usbpicprog.org](http://usbpicprog.org)
 
-The trunk folder (it's there for historical reasons) contains some subfolders:
+## Original Project Information ##
 
-* upp_wx: The PC application, also called usbpicprog-software
-* uc_code: The PIC firmware, also called usbpicprog-firmware
-* pcb: The PCB design, made in [KiCad](http://www.kicad-pcb.org/), for the PIC programmer in SMD, through hole and a zif socket
-* boot: The (slightly modified) Picdem Bootloader which has to be programmed once in the microcontroller on the usbpicprog hardware
-* doc: A bunch of programming specifications, provided by Microchip. 
+For more information please visit: [usbpicprog.org][1]
+
+The `trunk/` folder _(it's there for historical reasons)_ contains a handful of subfolders:
+
+* `upp_wx/`: The **PC** application, also called `usbpicprog-software`
+* `uc_code/`: The **PIC** firmware, also called `usbpicprog-firmware`
+* `pcb/`: The **PCB** design, made in [KiCad][3], for the PIC programmer in SMD, through-hole and a ZIF socket
+* `boot/`: The _(slightly modified)_ PICDEM bootloader, which has to be programmed once in the microcontroller on the
+  **usbpicprog** hardware
+* `doc/`: A bunch of programming specifications, provided by [Microchip][4]
+
+
+## Modifications, Enhancements, and Feature Updates ##
+
+This section will list the various **modifications**, **enhancements** and _(potential)_ **feature updates** that have
+been added to the project by me.
+
+
+### Modifications ###
+
+For the most part, the **modifications** will not cause any _functional_ change in the project, but is moreso aimed at
+improving the **source code formatting**, the **documentation**, and the **build/release** infrastructure; this should
+facilitate further development of the project.
+
+Some modifications that are either completed or planned are listed below:
+- Update documentation
+  - Ensure all source code is documented with [doxygen][5] compatible comments
+  - Improvements to the format of the contents of the `doc/` folder
+  - New documentation
+- Updates to the build scripts
+
+
+
+
+[1]:    <http://usbpicprog.org>
+[2]:    <https://github.com/fransschreuder/usbpicprog>
+[3]:    <http://www.kicad-pcb.org>
+[4]:    <https://www.microchip.com>
+[5]:    <http://doxygen.org>
 
--- trunk/release.sh
+++ trunk/release.sh
@@ -1,69 +1,76 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 
-if [ $# -ne 0 ]
-then
-  RELEASE=$1
+if [ $# -ne 0 ]; then
+	RELEASE=$1
 else
-  RELEASE=$(date +"%y%m%d")
-  RELEASE=${RELEASE:0:6}
+	RELEASE=$(date +%Y%m%d)
+	RELEASE=${RELEASE:0:6}
 fi
-echo Building release: $RELEASE
 
-if [ -d release ]
-then
-	echo Directory release exists, removing ...
+echo "Building release: $RELEASE"
+
+if [ -d release ]; then
+	echo -e "Directory \033[1mrelease\033[0m exists, removing..."
 	rm -rf release
 fi
 
-echo making release directories release/usbpicprog and release/firmware...
+echo -e "Making release directories \033[1mrelease/usbpicprog\033[0m and \033[1mrelease/firmware\033[0m for build..."
 mkdir release
 
 
-echo exporting git tree...
+echo "exporting git tree..."
 git clone ../ release/
-mkdir release/usbpicprog
-mkdir release/firmware
-mv release/trunk/upp_wx release/usbpicprog/usbpicprog-$RELEASE
-mv release/trunk/uc_code release/firmware/firmware-$RELEASE
-mv release/trunk/boot release/firmware/boot1.0
-rm -r release/trunk
-rm -r release/README.md
-rm -rf release/.git
 
-echo copy hexfiles...
-cp release/firmware/firmware-$RELEASE/uc_code.hex release/firmware/firmware-$RELEASE.hex
-cp release/firmware/boot1.0/Boot.hex release/firmware/boot1.0.hex
+mkdir "release/usbpicprog"
+mkdir "release/firmware"
+mv "release/trunk/upp_wx" "release/usbpicprog/usbpicprog-$RELEASE"
+mv "release/trunk/uc_code" "release/firmware/firmware-$RELEASE"
+mv "release/trunk/boot" "release/firmware/boot1.0"
+rm -r "release/trunk"
+rm -r "release/README.md"
+rm -rf "release/.git"
 
-echo making firmware zip archive...
-cd release/firmware
-zip -9 -r firmware-$RELEASE.zip *
-mv firmware-$RELEASE.zip ..
+echo -e "copying \033[1m*.hex\033[0m files..."
+cp "release/firmware/firmware-$RELEASE/uc_code.hex" "release/firmware/firmware-$RELEASE.hex"
+cp "release/firmware/boot1.0/Boot.hex" "release/firmware/boot1.0.hex"
+
+echo "making firmware zip archive..."
+cd "release/firmware"
+zip -9 -r "firmware-$RELEASE.zip" *
+mv "firmware-$RELEASE.zip" "../"
 cd ..
 
-echo creating autogen files
-cd usbpicprog/usbpicprog-$RELEASE
+echo "creating autogen files..."
+cd "usbpicprog/usbpicprog-$RELEASE"
+
 ./autogen.sh
-cp po/*.mo ..
+
+cp "po/*.mo" "../"
+
+# Run a make distclean to ensure we are starting fresh...
 make distclean
-echo restoring language files...
-mv ../*.mo po
+
+echo "restoring language files..."
+mv "../*.mo" "po/"
 rm -rf m4
 rm -rf autom4te.cache
+
 if [  ${RELEASE:0:2} = "0."  ]; then
-( echo '// generated file';
-    echo '#define UPP_VERSION "usbpicprog '$RELEASE'"'; )> svn_revision.h;
+	(echo '// generated file'; echo '#define UPP_VERSION "usbpicprog '$RELEASE'"';) > svn_revision.h;
 else
-( echo '// generated file';
-    echo '#define SVN_REVISION "usbpicprog '$RELEASE'"'; )> svn_revision.h;
+	(echo '// generated file'; echo '#define SVN_REVISION "usbpicprog '$RELEASE'"';) > svn_revision.h;
 fi
 
 cd ..
-echo creating source archive...
-tar -zcvhf usbpicprog-$RELEASE.tar.gz usbpicprog-$RELEASE
-mv usbpicprog-$RELEASE.tar.gz ..
+
+echo "creating source archive..."
+tar -zcvhf "usbpicprog-$RELEASE.tar.gz" "usbpicprog-$RELEASE"
+mv "usbpicprog-$RELEASE.tar.gz" "../"
 
 cd ..
 rm -rf firmware
 rm -rf usbpicprog
-echo done...
+
+echo -e "\n\n\033[33;1m--- FINISHED ---\033[0m\n\n"
+
